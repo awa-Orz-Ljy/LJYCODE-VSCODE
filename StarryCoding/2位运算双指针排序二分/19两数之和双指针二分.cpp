dce416b5fdac09e99037b4f10d2f2c3333a65092
awa_Orz_Ljy <20238131022@m.scnu.edu.cn>
2025-04-06 11:22:27 +0800

    11

--- StarryCoding/2位运算双指针排序二分/19两数之和双指针二分.cpp
+++ StarryCoding/2位运算双指针排序二分/19两数之和双指针二分.cpp
@@ -1,21 +1,22 @@
+//给一个大小为n的数组a和一个m，求符合a[i] + a[j] > m的ij二元组的对数
 #include<bits/stdc++.h>
 using namespace std;
 using ll = long long;
 int const N = 2e5 + 5;
-
+ll a[N];
 
 int main() {
     ll n, m; cin >> n >> m;
-    vector<ll> a(n + 1);
     for (ll i = 1; i <= n; i++) cin >> a[i];
-    sort(a.begin() + 1, a.end());
+    sort(a + 1,a + n + 1);
+
     ll ans = 0,l = 1, r = n;
         while (l + 1 != r) {
-            if (a[r] + a[l] > m){
+            if (a[r] + a[l] > m){//从大的数找会更快找到
             	ans += r - l;
             	r --;
             }
-            else l ++;//反之
+            else l ++;//反之，则移动l
         }
     cout << ans;
     return 0;
